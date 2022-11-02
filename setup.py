bde01b55289ca938111fc348f06c3591401f5f4f
Mamello Seboholi <j.seboholi@gmail.com>
2022-11-02 19:25:30 +0200

    chore: update setup url

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='cv_gmm_deep',
     version='0.0.1',
     description='Private package for GMM and Deep Learning utilities',
-    url='git@github.com:mamello-justice/cv-gmm-deep.git',
+    url='git@github.com:mamello-justice/cv_gmm_deep.git',
     author='Mamello Seboholi',
     author_email='j.seboholi@gmail.com',
     packages=['cv_gmm_deep']
